8da3e8bead5b770e4827eeaf27100831cb917ca2
Luka Korica <lukakorica@gmail.com>
2021-02-01 18:48:03 +0100

    Dodat vikend 30-31.1.2021.

---
+++ 2. godina/03. C#/01. Uvod/01. Konzolna aplikacija/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cout --> Console.ReadLine()
+            // cin  --> Console.WriteLine()
+            /*
+                Ovo je komentar
+                zapisan u vise
+                linija
+            */
+            //Console.WriteLine("Ovo je moj prvi programu C#");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Uneti broj je: " + a);
+
+            // // Tipovi promenljivih
+            // int broj = 5;           // long, unsigned
+            // double pi = 3.14159;    
+            // char slovo = 'A';
+            // string ime = "Luka";
+            // bool prover = true;
+
+
+            int x;
+            x = 5;
+            //int z = x + a;
+            //Console.WriteLine("a + 5 = " + z);
+
+            double razlomeljni = 5.999999;
+            int ceo = (int)razlomeljni; // kastujem double u int 5
+            ceo = (int)Math.Round(razlomeljni);
+            Console.WriteLine(ceo);
+
+            Console.WriteLine("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.WriteLine("Unesite godine: ");
+            int godine = Convert.ToInt32(Console.ReadLine());
+            if (godine >= 14)
+            {
+                Console.WriteLine("Dobrodosao, " + ime + "!\n");
+            }
+            else
+            {
+                Console.WriteLine("Begaj kidaro!");
+            }
+
+            // Nizovi 1D
+            //Console.WriteLine("Unesite dimenziju niza: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] A = new int[n];
+            //Console.WriteLine("Unesite clanove niza: ");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    A[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("Niz A: ");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine(A[i]);
+            //}
+
+            // Nizovi 2D (matrica)
+            Console.WriteLine("Unesite dimenziju niza: ");
+            int k = Convert.ToInt32(Console.ReadLine().Split());
+            int l = Convert.ToInt32(Console.ReadLine().Split());
+            int[,] M = new int[k, l];
+            Console.WriteLine("Unesite clanove niza: ");
+            for (int i = 0; i < k; i++)
+            {
+                for(int j = 0; j < l; j++)
+                {
+                    M[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Niz A: ");
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < l; j++)
+                {
+                    Console.WriteLine(M[i, j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2. godina/03. C#/01. Uvod/02. Forme/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uvod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string t1 = textBox1.Text;      // A
+            string t2 = textBox2.Text;      // B
+            string t3 = textBox3.Text;
+            double a, b, c;
+            //double z = a + b;
+            // MessageBox.Show(Convert.ToString(z));
+            // MessageBox.Show("Rezultat sabiranja a i b je: " + z);
+            //label4.Text = Convert.ToString(z);
+            double rez = 0;
+            if(radioButton1.Checked == true)
+            {
+                a = Convert.ToDouble(t1);
+                b = Convert.ToDouble(t2);
+                // Pravougaonik
+                rez = a * b;
+                label4.Text = Convert.ToString(rez);
+            }
+            else if(radioButton2.Checked == true)
+            {
+                a = Convert.ToDouble(t1);
+                // Krug
+                rez = a * a * Math.PI;
+                label4.Text = Convert.ToString(rez);
+            }
+            else if (radioButton3.Checked)
+            {
+                a = Convert.ToDouble(t1);
+                b = Convert.ToDouble(t2);
+                c = Convert.ToDouble(t3);
+                // Kvadar
+                rez = 2 * (a * b) + 2 * (a * c) + 2 * (b * c);
+                label4.Text = Convert.ToString(rez);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            textBox1.Visible = true;
+            label2.Visible = true;
+            textBox2.Visible = true;
+            label3.Visible = false;
+            textBox3.Visible = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            textBox1.Visible = true;
+            label2.Visible = false;
+            textBox2.Visible = false;
+            label5.Visible = false;
+            textBox3.Visible = false;
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            // Kvadar
+            label1.Visible = true;
+            textBox1.Visible = true;
+            label2.Visible = true;
+            textBox2.Visible = true;
+            label5.Visible = true;
+            textBox3.Visible = true;
+
+        }
+    }
+}
