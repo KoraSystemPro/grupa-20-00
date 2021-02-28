ce6456732e1ecec8561da36c5ae314605b5ade79
Luka Korica <lukakorica@gmail.com>
2021-03-01 00:14:38 +0100

    Dodat 28-FEB-2021

---
+++ 2. godina/03. C#/05. Zmijica/Zmijica/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmijica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // █ - 219
+            // ▄ - 220
+
+            // Boja konzole
+            Console.BackgroundColor = ConsoleColor.White;
+            // Boja zmije
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            // Ako drzite alt i ukucate neki od ova dva koda
+            // dobicete odgovarajucu figuru
+            Console.WindowHeight = 16;
+            Console.WindowWidth = 32;
+            int prozorSirina = Console.WindowWidth;
+            int prozorVisina = Console.WindowHeight;
+            Random nasumicanBroj = new Random();
+            int score = 3;
+            string smer = "LEVO";
+
+            int glavaX = prozorSirina / 2;
+            int glavaY = prozorVisina / 2;
+            List<int> teloX = new List<int>();
+            List<int> teloY = new List<int>();
+            int vockaX = nasumicanBroj.Next(1, prozorSirina-1);
+            int vockaY = nasumicanBroj.Next(1, prozorVisina-1);
+
+
+            int gameover = 0;
+            while (true)
+            {
+                // "Resetuje frejm"
+                Console.Clear();
+
+                // Proverava da li je zmija udarila u ivicu
+                if(glavaX == prozorSirina-1 || glavaX == 0 || glavaY == prozorVisina-1 || glavaY == 0)
+                {
+                    gameover = 1;
+                }
+
+                // Crta gornju ivicu
+                for (int i = 0; i < prozorSirina; i++)
+                {
+                    Console.SetCursorPosition(i, 0);
+                    Console.Write('▄');
+                }
+                // Crta donju ivicu
+                for (int i = 0; i < prozorSirina; i++)
+                {
+                    Console.SetCursorPosition(i, prozorVisina-1);
+                    Console.Write('▄');
+                }
+                // Crta levu ivicu
+                for (int i = 0; i < prozorVisina; i++)
+                {
+                    Console.SetCursorPosition(0, i);
+                    Console.Write('▄');
+                }
+                // Crta desnu ivicu
+                for (int i = 0; i < prozorVisina; i++)
+                {
+                    Console.SetCursorPosition(prozorSirina-1, i);
+                    Console.Write('▄');
+                }
+
+                // Ako pokupimo vocku, uvecavamo skor i pravimo novu
+                if(vockaX == glavaX && vockaY == glavaY)
+                {
+                    score++;
+                    vockaX = nasumicanBroj.Next(1, prozorSirina-1);
+                    vockaY = nasumicanBroj.Next(1, prozorVisina-1);
+                }
+
+                // Nacrtaj zmijicu
+                Console.SetCursorPosition(glavaX, glavaY);
+                Console.Write('▄');
+
+                if (gameover == 1)
+                {
+                    break;
+                }
+
+                Console.ReadKey();
+
+
+                switch (smer)
+                {
+                    case "GORE":
+                        glavaY--;
+                        break;
+                    case "DESNO":
+                        glavaX++;
+                        break;
+                    case "LEVO":
+                        glavaX--;
+                        break;
+                    case "DOLE":
+                        glavaY++;
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
